c291210d92f7f295d3e021dd2a3aae76b6c3752b
Sabrina Lamont <sabrina.lamont@tmsw.com>
2022-10-04 21:41:00 +0100

    create big wheel

---
+++ src/components/BigWheel/BigWheel.js
@@ -0,0 +1,28 @@
+import './BigWheel.scss'
+
+export default function BigWheel() {
+
+    return (
+        <div className="big-wheel">
+            <div className="love-section">
+                Love
+            </div>
+            <div className="happy-section">
+                Happiness
+            </div>
+            <div className="well-section">
+                Well Being
+            </div>
+            <div className="pos-section">
+                Positivity
+            </div>
+            <div className="joy-section">
+                Joyful
+            </div>
+            <div className="excit-section">
+                Excitement
+            </div>
+        </div>
+    )
+
+}
